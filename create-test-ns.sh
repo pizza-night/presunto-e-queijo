7b6f9e13f706d61914b61391b67c3371ec1c9739
mendess <pedro.mendes.26@gmail.com>
2023-05-28 11:39:00 +0100

    clean up test scripts

--- create-test-ns.sh
+++ create-test-ns.sh
@@ -4,7 +4,6 @@ clients=(peq1 peq2 peq3)
 
 bash -x ./drop-test-ns.sh
 ip --color=always addr
-sleep 1
 
 for c in ${clients[@]}; do
     sudo ip netns add $c
@@ -16,13 +15,12 @@ sudo ip link set dev br0 up
 ci=1
 for c in ${clients[@]}; do
     sudo ip link add veth-$c type veth peer name veth-$c-host
+
     sudo ip link set veth-$c netns $c
     sudo ip link set veth-$c-host master br0
     sudo ip link set veth-$c-host up
     sudo ip netns exec $c ip link set veth-$c up
     sudo ip netns exec $c ip addr add 10.1.1.$((ci++))/24 dev veth-$c
-    #sudo ip addr add 10.1.1.$((++ci))/24 dev veth-$c-host
-    # sudo ip netns exec $c ip route add default via 10.1.1.$ci
 done
 
 sudo ip addr add 10.1.1.254/24 dev br0
--- dos.sh
+++ dos.sh
@@ -3,9 +3,13 @@
 
 total_clients=0
 batch_size=1
+ip=10.1.1.1
+if [ "$1" ]; then
+    ip="$1"
+fi
 while :; do
     for _ in $(seq 1 $batch_size); do
-        netcat 10.1.1.1 2504 < /dev/random >/dev/null &
+        netcat "$ip" 2504 < /dev/random >/dev/null &
         ((++total_clients))
     done
     ((++batch_size))
